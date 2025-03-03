60357578987fc1f2790190b9daf9888ff52ffc62
Max de Danschutter <max.de.danschutter@traverseresearch.nl>
2025-03-03 10:26:20 +0100

    disable layout tests

--- steamworks-sys/build.rs
+++ steamworks-sys/build.rs
@@ -70,6 +70,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             )
             .allowlist_item("SteamAPI\\w+")
             .dynamic_library_name("steam_api")
+            // https://github.com/rust-lang/rust-bindgen/issues/3166
+            .layout_tests(false)
             .clang_arg("-xc++")
             .clang_arg("-std=c++11")
             .clang_arg(format!("-I{}", sdk_src.join("public").display()))
